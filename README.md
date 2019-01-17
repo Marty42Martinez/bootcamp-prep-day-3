edae100a206bb6d6a878d00d93fcffb465106af7
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-01-16 20:17:52 -0800

    playing with JS, learning Control Flow...You notice that you are being followed by a flock of brightly colored songbirds

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bootcamp-prep-day-3
 We startin' JavaScript, yo
+
+Note: we set "no-console" variable from "warn" to "off" in .eslintrc
--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-var sum = 3 + 4;
-console.log('hello console!', 7, sum);
+var x = 16;
+
+if(x > 12) { 
+    console.log(x);
+    console.log('groovin with if');
+} else {
+    console.log('sorry, x is too small')
+}
+
+//1. Initialize
+//2. Condition
+//3. Post Loop Action
+for(var i = 0; i < x; i++) {
+    if(i % 2 === 0) {
+        console.log(i, " rabid bunnies charge you");
+    }
+}
+
+function sayHello() {
+    console.log('Well, hello there marty')
+    console.log('It sure seems like lots of rabbits are after you...')
+}
+if(x % 2 === 0) {
+    sayHello();
+}
